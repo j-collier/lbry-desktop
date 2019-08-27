6a841c9513d67fae87daf96c7d818bd647bc5ba0
Sean Yesmunt <sean@lbry.io>
2019-08-27 10:43:42 -0400

    add createChannel to first run flow

--- src/ui/component/page/view.jsx
+++ src/ui/component/page/view.jsx
@@ -1,16 +1,30 @@
 // @flow
-import * as React from 'react';
+import type { Node } from 'react';
+import * as ICONS from 'constants/icons';
+import React, { Fragment } from 'react';
 import classnames from 'classnames';
+import SideBar from 'component/sideBar';
+import Header from 'component/header';
+import Button from 'component/button';
 
 type Props = {
-  children: React.Node | Array<React.Node>,
+  children: Node | Array<Node>,
   className: ?string,
+  fullscreen: boolean,
 };
 
 function Page(props: Props) {
-  const { children, className } = props;
+  const { children, className, fullscreen = false } = props;
 
-  return <main className={classnames('main', className)}>{children}</main>;
+  return (
+    <Fragment>
+      <Header minimal={fullscreen} />
+      <div className={classnames('main-wrapper__inner')}>
+        <main className={classnames('main', className)}>{children}</main>
+        {!fullscreen && <SideBar />}
+      </div>
+    </Fragment>
+  );
 }
 
 export default Page;
