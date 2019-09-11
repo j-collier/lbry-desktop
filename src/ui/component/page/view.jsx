867cd034ee13558fd3fd29b0fe9ae4870b70fd8d
Sean Yesmunt <sean@lbry.io>
2019-09-11 10:30:35 -0400

    page style fix

--- src/ui/component/page/view.jsx
+++ src/ui/component/page/view.jsx
@@ -1,27 +1,27 @@
 // @flow
 import type { Node } from 'react';
-import * as ICONS from 'constants/icons';
 import React, { Fragment } from 'react';
 import classnames from 'classnames';
 import SideBar from 'component/sideBar';
 import Header from 'component/header';
-import Button from 'component/button';
 
 type Props = {
   children: Node | Array<Node>,
   className: ?string,
   fullscreen: boolean,
+  authenticated: boolean,
 };
 
 function Page(props: Props) {
-  const { children, className, fullscreen = false } = props;
+  const { children, className, fullscreen = false, authenticated } = props;
+  const showSideBar = !fullscreen && (IS_WEB ? authenticated : true);
 
   return (
     <Fragment>
       <Header minimal={fullscreen} />
       <div className={classnames('main-wrapper__inner')}>
-        <main className={classnames('main', className)}>{children}</main>
-        {!fullscreen && <SideBar />}
+        <main className={classnames('main', className, { 'main--full-width': !showSideBar })}>{children}</main>
+        {showSideBar && <SideBar />}
       </div>
     </Fragment>
   );
